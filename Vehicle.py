4df149fe8d8a2dec153c77f1c7f7e3751fd95355
Jayanta kumar sahoo <sahoochiku112@gmail.com>
2021-02-19 12:26:15 +0530

    create car for parkinglot

---
+++ Vehicle.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self,reg_no ,colour):
+        self.reg_no = reg_no
+        self.colour = colour
+
+    def reg_no(self):
+        return self.reg_no
+
+    def colour(self):
+        return self.colour
